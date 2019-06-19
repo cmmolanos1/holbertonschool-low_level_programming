231228d86a35c34fd95ed62b40620523814cb5f6
Carlos Molano <cmmolanos@gmail.com>
2019-06-19 11:06:07 -0500

    Uploading basic Task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+	char typeChar;
+	int typeInt;
+	long typeLongInt;
+	long long typeLongLongInt;
+	float typeFloat;
+	/* With sizeof() we get the number of bytes */
+	printf("Size of a char: %lu byte(s)\n", sizeof(typeChar));
+	printf("Size of an int: %lu byte(s)\n", sizeof(typeInt));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(typeLongInt));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(typeLongLongInt));
+	printf("Size of a float: %lu byte(s)\n", sizeof(typeFloat));
+
+	return (0);
+}
