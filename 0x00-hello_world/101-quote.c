c29a651305bbdffa94b4d69673106f04acdfb648
Carlos Molano <cmmolanos@gmail.com>
2019-06-19 14:07:45 -0500

    Fixing 101 2

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <unistd.h>
+#include <stdio.h>
 #include <string.h>
 /**
  * main - Entry point
@@ -8,7 +9,9 @@
 int main(void)
 {
 	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
 
-	write(2, quote, strlen(quote));
+	write(2, quote, 59);
+	printf("%i",length);
 	return (1);
 }
