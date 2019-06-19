396f2522e7df8c637dd46e3cf2ad6b02dca2f583
Carlos Molano <cmmolanos@gmail.com>
2019-06-19 13:49:59 -0500

    Uploading 101-quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(2, quote, strlen(quote));
+	return (1);
+}
