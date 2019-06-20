841cacdfe9d65162b1f249a19401b4cb479537ca
Carlos Molano <cmmolanos@gmail.com>
2019-06-20 12:48:29 -0500

    Fixing 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,12 @@ int main(void)
 {
 	int i;
 	int j;
-	
-	for (i=48; i<58; i++)
+
+	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-	for (j=97; j<103; j++)
+	for (j = 97; j < 103; j++)
 	{
 		putchar(j);
 	}
