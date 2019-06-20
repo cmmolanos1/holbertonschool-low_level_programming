11b575b726b32f27eca092b5c4beb5167b01f77a
Carlos Molano <cmmolanos@gmail.com>
2019-06-20 15:03:23 -0500

    Uploading 101

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if (i < j && j < k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i < '7' || j < '8' || k < '9')
+					{
+						putchar(44);
+						putchar(32);
+					}
+					else
+					{
+						putchar('\n');
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
