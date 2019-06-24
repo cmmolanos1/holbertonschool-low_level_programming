fa143ddd4ca5c36c0107643fb3ce114339aaceb6
Carlos Molano <cmmolanos@gmail.com>
2019-06-24 07:13:55 -0500

    First upload

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * main - Read charcters and prints using _char
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
+
+	return(0);
+}
