b661d7499ac37373c048cfb74c7ac7ffec08cd95
Carlos Molano <cmmolanos@gmail.com>
2019-06-24 23:49:00 -0500

    Uploading 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_times_table - print the time tables
+ *@n: the tested number
+ *
+ * Return: the time tables from 0 to n
+ */
+void print_times_table(int n)
+{
+	int j, k, num, units, tens, cents;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (j = 0; j <= n; j++)
+		{
+			_putchar('0');
+			for (k = 1; k <= n; k++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				num = j * k;
+				cents = num / 100;
+				tens = (num / 10) % 10;
+				units = num % 10;
+
+				if (num > 99)
+				{
+					_putchar(cents + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				if (num > 9)
+				{
+					_putchar(tens + '0');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+				_putchar(units + '0');
+				if (k == n)
+				{
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
