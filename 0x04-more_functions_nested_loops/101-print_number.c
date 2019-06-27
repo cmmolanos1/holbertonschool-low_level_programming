9dc4ac833a2145471ab7362935e94c7b0d6ac7f3
Carlos Molano <cmmolanos@gmail.com>
2019-06-27 15:55:07 -0500

    uploading 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,57 @@
+#include "holberton.h"
+/**
+ * print_number - print an int numbers.
+ * @n: number tested
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	int i, j, digits, numchar, power;
+
+	digits = numDigits(n);
+
+	power = 1;
+
+	for (i = 1; i < digits; i++)
+	{
+		power = power * 10;
+	}
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else
+	{
+		;
+	}
+	for (j = power; j >= 1; j = j / 10)
+	{
+		numchar = (n / j) % 10;
+		_putchar(numchar + '0');
+	}
+}
+/**
+ * numDigits - show the number or digits
+ * @a: number to check
+ * Return: number of digits
+ */
+int numDigits(int a)
+{
+	int digit;
+	int number;
+
+	digit = 0;
+	if (a < 0)
+	{
+		a = -a;
+		}
+	number = a;
+		while (number >= 10)
+		{
+			number = number / 10;
+			digit++;
+		}
+		return (digit + 1);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -26,4 +26,8 @@ void print_triangle(int size);
 int isPrime(long int n);
 
 long int biggestFactor(long int a);
+
+void print_number(int n);
+
+int numDigits(int n);
 #endif
