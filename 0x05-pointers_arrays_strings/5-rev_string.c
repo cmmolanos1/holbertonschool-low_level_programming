c5570615ca7bfe3a263d95690541b47a81aba0bf
Carlos Molano <cmmolanos@gmail.com>
2019-07-02 05:31:38 -0500

    Fixing 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,13 @@ void rev_string(char *s)
 {
 	int length, j;
 	char a;
+
 	length = 0;
-	
 	while (*(s + length) != '\0')
 	{
 		length++;
 	}
-	
-	for(j = 0; j < (length / 2); j++)
+	for (j = 0; j < (length / 2); j++)
 	{
 		a = *(s + j);
 		*(s + j) = *(s + length - j - 1);
