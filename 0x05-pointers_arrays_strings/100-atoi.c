028b1a5ac4b4b2805ef0ecf144d0fadc767d6fbd
Carlos Molano <cmmolanos@gmail.com>
2019-07-01 19:24:21 -0500

    Uploading file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _atoi - print the integer of a char.
+ * @s:  tested char
+ * Return: integer.
+ */
+int _atoi(char *s)
+{
+	unsigned int counter, i, j, k, length, num, l;
+	int aux, aux2;
+
+	aux = 1;
+	aux2 = 1;
+	counter = 0;
+	num = 0;
+
+	while (*(s + counter) != '\0')
+		counter++;
+	for (i = 0; i < counter; i++)
+	{
+		if (*(s + i) <= '9' && *(s + i) >= '0')
+		break;
+	}
+	for (j = i; j < counter; j++)
+	{
+		if (!(*(s + j) <= '9' && *(s + j) >= '0'))
+			break;
+		aux2 = aux2 * 10;
+	}
+	aux2 = aux2 / 10;
+	for (k = 0; k < i; k++)
+	{
+		if (*(s + k) == '-')
+			aux = -aux;
+	}
+	length = j - i;
+	l = i;
+		while (length >= 1)
+	{
+		num = num + ((*(s + l) - '0') * aux2);
+		l++;
+		length--;
+		aux2 = aux2 / 10;
+	}
+	return (num * aux);
+}
