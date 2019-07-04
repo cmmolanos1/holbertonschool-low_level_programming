27ecc5cf27cb5d2d4d0d536ff5620917878e9ae9
Carlos Molano <cmmolanos@gmail.com>
2019-07-04 12:04:43 -0500

    Uploading holberton

--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,7 +1,7 @@
 #ifndef INV_HOLBERTON_H
 #define INV_HOLBERTON_H
 
-nt _putchar(char);
+int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
