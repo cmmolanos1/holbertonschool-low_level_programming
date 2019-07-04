e95868523a275859ba07f9d37518c83621e084a8
Carlos Molano <cmmolanos@gmail.com>
2019-07-04 18:50:10 -0500

    Uploading 102

---
+++ 0x06-pointers_arrays_strings/102-infinite_add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * infinite_add - add 2 strings.
+ * @n1: string1.
+ * @n2: string2.
+ * @r: buffer
+ * @size_r: buffer size
+ * Return: String with all letters in ROT13 base.
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+       ;
+}
