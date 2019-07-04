3617f08099ff564b7d80d2bc2f06fddbec032a5d
Carlos Molano <cmmolanos@gmail.com>
2019-07-04 16:30:57 -0500

    Uploading 101

--- 0x06-pointers_arrays_strings/101-magic.c
+++ 0x06-pointers_arrays_strings/101-magic.c
@@ -1,17 +1,13 @@
 #include <stdio.h>
-/**
- * main - print 98.
- *
- * Return: Always 0.
- */
+
 int main(void)
 {
-	int n;
-	int a[5];
-	int *p;
+  int n;
+  int a[5];
+  int *p;
 
-	a[2] = 1024;
-	p = &n;
+  a[2] = 1024;
+  p = &n;
   /*
    * write your line of code here...
    * Remember:
@@ -20,8 +16,8 @@ int main(void)
    * - only one statement
    * - you are not allowed to code anything else than this line of code
    */
-	*(a + 2) = sizeof(n) + 94;
+  p[5] = 98;
   /* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+  printf("a[2] = %d\n", a[2]);
+  return (0);
 }
