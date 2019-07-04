fd350b031c72d6e515b9c74083c416f048445487
Carlos Molano <cmmolanos@gmail.com>
2019-07-04 13:10:17 -0500

    Fixing 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	while (*s1 != '\0' && *s2 != '\0')
+	while (( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2)
 	{
 		s1++;
 		s2++;
@@ -18,17 +18,6 @@ int _strcmp(char *s1, char *s2)
 	}
 	else
 	{
-		if (*s1 == '\0')
-		{
-			return ('0' - *s2);
-		}
-		else if (*s2 == '\0')
-		{
-			return (*s1 - '0');
-		}
-		else
-		{
-		return (*s1 - *s2);
-		}
+	return (*s1 - *s2);
 	}
 }
