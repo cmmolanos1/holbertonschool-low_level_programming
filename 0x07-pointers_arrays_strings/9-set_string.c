466640ef02017ee0abb3aa62bddcd2698c20d45b
Carlos Molano <cmmolanos@gmail.com>
2019-07-08 15:02:48 -0500

    Uploading file

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * set_string - print additions of the two diagonals.
+ * @a: source matrix
+ * @size: size NxN of matrix
+ *
+ * Return: chessboard.
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;	
+}
