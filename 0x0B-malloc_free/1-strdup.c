816d70fbe2c55fd03270a1b618c872b1757f5067
Carlos Molano <cmmolanos@gmail.com>
2019-07-16 13:01:33 -0500

    Fixing 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,24 +12,17 @@ char *_strdup(char *str)
 {
 	int strSize, i;
 	char *copyStr;
-
-	if (!*str)
-	{
+	strSize = 0;
+	
+	if (str == NULL)
 		return (NULL);
-	}
-	else
-	{
-		strSize = 0;
-		while (*(str + strSize))
-			strSize++;
-		copyStr = malloc((strSize + 1) * sizeof(char));
-		if (copyStr == NULL)
-			return (0);
-		for (i = 0; i < (strSize + 1); i++)
-		{
-			*(copyStr + i) = *(str + i);
-		}
-		return (copyStr);
-		free(copyStr);
-	}
+	while (*(str + strSize))
+		strSize++;
+	strSize++; /* add 1 to print NULL*/
+	copyStr = malloc((strSize) * sizeof(char));
+	if (copyStr == NULL)
+		return (0);
+	for (i = 0; i < (strSize); i++)
+		*(copyStr + i) = *(str + i);
+	return (copyStr);
 }
