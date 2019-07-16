6205b021fba2937fffeee5ff70d2095852e0c840
Carlos Molano <cmmolanos@gmail.com>
2019-07-16 13:54:44 -0500

    Fixing 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,16 +17,25 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	matrix = (int **) malloc(height * sizeof(int));
-
 	if (matrix == NULL)
+	{
+		free(matrix);
 		return (NULL);
+	}
 
 	for (i = 0; i < height; i++)
-		matrix[i] = (int *) malloc(width * sizeof(int));
-
+	{
+		*(matrix + i) = (int *) malloc(width * sizeof(int));
+		if ((matrix + i) == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(matrix + i);
+			free(matrix);
+		}
+	}
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
-			matrix[i][j] = 0;
+			*(*(matrix + i) +j) = 0;
 
 	return (matrix);
 }
