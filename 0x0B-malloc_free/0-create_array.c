bb5d8fb15a6640fac72c075ef816c918ab7fd780
Carlos Molano <cmmolanos@gmail.com>
2019-07-16 12:53:43 -0500

    Fixing 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,9 +16,9 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	arrayMem = malloc(size * sizeof(char));
+	if (arrayMem == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
-	{
 		*(arrayMem + i) = c;
-	}
 	return (arrayMem);
 }
