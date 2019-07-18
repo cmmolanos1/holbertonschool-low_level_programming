3913f9c9619e92ed6c81a04ceec67d6cee7337ea
Carlos Molano <cmmolanos@gmail.com>
2019-07-18 15:31:32 -0500

    Uploading 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -29,6 +29,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	for (i = 0; i < new_size; i++)
 		*(reallocated + i) = *(ptrCast + i);
 
-	free (ptr);
+	free(ptr);
 	return (reallocated);
 }
