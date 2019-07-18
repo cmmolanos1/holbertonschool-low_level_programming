a30df34b9ada39dbd1f631fb1c7d6cdf97bf23a3
Carlos Molano <cmmolanos@gmail.com>
2019-07-18 15:29:33 -0500

    Uploading 100-realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _realloc - change the memory space of an array.
+ * @ptr: pointer to old array.
+ * @old_size: the size of ptr
+ * @new_size: new size of array
+ * Return: pointer to the new array.
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int i;
+	char *reallocated, *ptrCast;
+
+	ptrCast = ptr;
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	reallocated = malloc(new_size);
+	if (reallocated == NULL)
+		return (NULL);
+
+	for (i = 0; i < new_size; i++)
+		*(reallocated + i) = *(ptrCast + i);
+
+	free (ptr);
+	return (reallocated);
+}
