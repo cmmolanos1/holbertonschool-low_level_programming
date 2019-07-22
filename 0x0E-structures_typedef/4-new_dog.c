3d6fa20ee1daa58049eb1de7ad7bb8ff88deebb5
Carlos Molano <cmmolanos@gmail.com>
2019-07-22 14:19:17 -0500

    Uploading files

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,13 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
- * new dog - creates a new dog.
+ * new_dog - creates a new dog.
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
- * Return: content.
+ * Return: pointer to the new dog.
  */
-dog_t *new_dog(char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner)
 {
-	
+	dog_t *newDog;
+	int lName, lOwner, i;
+
+	newDog = (dog_t *)malloc(sizeof(dog_t));
+	if (newDog == NULL)
+		return (NULL);
+
+	for (lName = 0; *name; lName++)
+		name++;
+	lName++; /* add 1 for null*/
+	for (lOwner = 0; *owner; lOwner++)
+		owner++;
+	lOwner++;/*add 1 for null*/
+
+	newDog->name = malloc(lName * sizeof(char));
+	if (newDog->name == NULL)
+	{
+		free(newDog);
+		return (NULL);
+	}
+	for (i = 0; i <= lName; i++)
+		*(newDog->name + i) = *(name + i);
+
+	newDog->age = age;
+
+	newDog->owner = malloc(lOwner * sizeof(char));
+	if (newDog->owner == NULL)
+	{
+		free(newDog->name);
+		free(newDog);
+		return (NULL);
+	}
+	for (i = 0; i <= lName; i++)
+	*(newDog->name + i) = *(name + i);
+
+	return (newDog);
 }
