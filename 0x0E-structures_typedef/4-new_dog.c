431ea4be2ef4767b0665748086760fbd33cb06b1
Carlos Molano <cmmolanos@gmail.com>
2019-07-22 15:15:00 -0500

    Fixing 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,34 +13,36 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog_t *newDog;
 	int lName, lOwner, i;
 
-	newDog = (dog_t *)malloc(sizeof(dog_t));
+	newDog = malloc(sizeof(struct dog));
 	if (newDog == NULL)
 		return (NULL);
 
-	for (lName = 0; *name; lName++)
-		name++;
-	for (lOwner = 0; *owner; lOwner++)
-		owner++;
+	for (lName = 0; *(name + lName); lName++)
+		;
+	lName++;
+	for (lOwner = 0; *(owner + lOwner); lOwner++)
+		;
+	lOwner++;
 
-	newDog->name = malloc((lName + 1) * sizeof(char));
+	newDog->name = malloc(lName * sizeof(char));
 	if (newDog->name == NULL)
 	{
 		free(newDog);
 		return (NULL);
 	}
-	for (i = 0; i <= lName; i++)
+	for (i = 0; i < lName; i++)
 		*(newDog->name + i) = *(name + i);
 
 	newDog->age = age;
 
-	newDog->owner = malloc((lOwner + 1) * sizeof(char));
+	newDog->owner = malloc(lOwner * sizeof(char));
 	if (newDog->owner == NULL)
 	{
 		free(newDog->name);
 		free(newDog);
 		return (NULL);
 	}
-	for (i = 0; i <= lOwner; i++)
+	for (i = 0; i < lOwner; i++)
 	*(newDog->owner + i) = *(owner + i);
 
 	return (newDog);
