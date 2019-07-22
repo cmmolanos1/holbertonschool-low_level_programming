acd563099a47b0d1243f9f2f0708061dcf5d28cf
Carlos Molano <cmmolanos@gmail.com>
2019-07-22 15:52:46 -0500

    Updating files

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,6 +9,7 @@ void free_dog(dog_t *d);
 {
 	if (!d)
 		return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,11 @@
-#ifndef INV_DOG_H
-#define INV_DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
+
 /**
- * struct dog - dog attributes
- * @name: The dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- *
- * Description: Longer description
+ * struct dog - information about dogs
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
  */
 struct dog
 {
@@ -14,13 +13,16 @@ struct dog
 	float age;
 	char *owner;
 };
+
 /**
- * dog_t - typedef for struct dog
+ * dog_t - Typedef for struct dog
  */
 typedef struct dog dog_t;
 
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-#endif
+
+#endif /* _DOG_H_ */
