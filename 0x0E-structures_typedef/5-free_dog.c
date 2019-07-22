d7255614c51d44fdeed4cc78d11b047c5698af60
Carlos Molano <cmmolanos@gmail.com>
2019-07-22 15:37:51 -0500

    Uploading 5-free_dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees the memory of dog struct.
+ * @d: dog struct
+ * Return: pointer to the new dog.
+ */
+void free_dog(dog_t *d);
+{
+	if (!d)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
