9153c38978a29516b8a2ee3617ca1cd0de9fb8c3
Carlos Molano <cmmolanos@gmail.com>
2019-07-24 15:21:07 -0500

    Uploading Task #3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * main - operates two numbers.
+ * @argc: argument counter.
+ * @argv: argument vector.
+ * Return: result, otherwise 98 for arg error, 99 for op, error 100 for divide
+ * or multiply by 0
+ */
+int main(int argc, char **argv)
+{
+	int a, b, answer;
+	int (*function)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	if (argv[2][1] != '\0')
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	function = get_op_func(*(argv + 2));
+	if (!function)
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	answer = function(a, b);
+	printf("%d\n", answer);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - adds two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - substracts two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplicates two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divides two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+	else
+		return (a / b);
+}
+/**
+ * op_mod - obteins the module between two number.
+ * @a: first number.
+ * @b: second number.
+ * Return: result.
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+	return (a % b);
+}
