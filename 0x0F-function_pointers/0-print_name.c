e64e08bf131a208c592fe2484410465943e8ea41
Carlos Molano <cmmolanos@gmail.com>
2019-07-24 13:01:47 -0500

    Fixing 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,6 @@
-#include "function_pointers.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "function_pointers.h"
 /**
  * print_name - prints a name depends of the function.
  * @name: points to Name's string.
