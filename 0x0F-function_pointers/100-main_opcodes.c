8bb9d2fbf04fc27686e4fa028b8cae3d88430fb3
Carlos Molano <cmmolanos@gmail.com>
2019-07-24 19:35:05 -0500

    Fixing 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -9,16 +9,29 @@
  */
 int main(int argc, char **argv)
 {
+	int i, number;
+
+	number = atoi(*(argv + 1));
+
 	if (argc != 2)
 	{
 		puts("Error");
 		exit(1);
 	}
+
 	if (atoi(argv[1]) < 0)
 	{
 		puts("Error");
 		exit(2);
 	}
 
+	for (i = 0; i < number; i++)
+	{
+		printf("%02hhx ", ((char *)main)[i]);
+		if (i == (number - 1))
+		{
+			printf("%02hhx\n", ((char *)main)[i]);
+		}
+	}
 	return (0);
 }
