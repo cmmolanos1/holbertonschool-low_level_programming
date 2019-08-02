249448ff5e0f4d610e73ba9d34a932ded318afa5
Carlos Molano <926@holbertonschool.com>
2019-08-01 23:18:25 -0500

    [fix] 0-print_list.c: adding condition for str = NULL

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,9 +10,14 @@ size_t print_list(const list_t *h)
 	unsigned int i;
 
 	i = 0;
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
-		printf("[%u] %s\n", h->len, h->str);
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
 		i++;
 	}
