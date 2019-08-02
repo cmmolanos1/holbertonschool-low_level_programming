46d372ae0ec3f8f0d35b89116c5e6f2251d25e15
Carlos Molano <926@holbertonschool.com>
2019-08-01 23:00:29 -0500

    [feature] 100-first-c: uploading file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+/**
+ * printBefore - free memory of all list.
+ * void: pointer to head in fucntion
+ * Return: Always success.
+ */
+void printBefore(void) __attribute__ ((constructor));
+
+void printBefore(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
