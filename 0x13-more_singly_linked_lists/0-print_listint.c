de0c8582fd333b194cab87829f457385f715419a
Carlos Molano <926@holbertonschool.com>
2019-08-06 12:19:36 -0500

    Fixing bug 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -6,13 +6,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t j = 0;
+	int i = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		j++;
+		i++;
 	}
-	return (j);
+	return (i);
 }
