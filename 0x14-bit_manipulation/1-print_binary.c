5d1a2d1416d62d69ab53a1f8c0fb271929641b2a
Carlos Molano <926@holbertonschool.com>
2019-08-08 13:46:37 -0500

    [Fix] 1-print_binary: fixing bugs

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,23 +10,19 @@ void print_binary(unsigned long int n)
 	unsigned long int mask = 0;
 
 	if (n == 0)
-	{
 		_putchar('0');
-	}
-	else
+	for (i = 63; i >= 0; i--)/*Ignoring left-zeros*/
 	{
-		for (i = 63; i >= 0; i--)/*Ignoring left-zeros*/
-		{
-			mask = 1 << i;
-			digit = (n & mask) >> i;
-			if (digit == 1)
-				break;
-		}
-		for (; i >= 0; i--)
-		{
-			mask = 1 << i;/*Creating a mask for extract the digit*/
-			digit = (n & mask) >> i;/*Extract the digit and push to 0 or 1*/
-			_putchar(digit + '0');
-		}
+		mask = 1 << i;
+		digit = (n & mask) >> i;
+		if (digit == 1)
+			break;
+	}
+	for (; i >= 0; i--)
+	{
+		mask = 1 << i;/*Creating a mask for extract the digit*/
+		digit = (n & mask) >> i;/*Extract the digit and push to 0 or 1*/
+		_putchar(digit + '0');
 	}
 }
+
