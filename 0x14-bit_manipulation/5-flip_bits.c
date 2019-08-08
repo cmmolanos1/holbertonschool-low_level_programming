48253730315f2aef4de04a0dc8c333b566ac2da5
Carlos Molano <926@holbertonschool.com>
2019-08-08 11:28:23 -0500

    [Feature] 5-flip_bits.c: uploading file

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * flip_bits - returns the differeces about digits between 2 numbers.
+ * @n: the input number1
+ * @m: input number 2
+ * Return: the diferences
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int bit_diff, mask;
+	int i, digit;
+	unsigned int differences = 0;
+
+	bit_diff = n ^ m;/*1 if bit_n != bit_m (XOR)*/
+
+	for (i = 63; i >= 0; i--)
+	{
+		mask = 1 << i;/*Creating a mask for extract the digit*/
+		digit = (bit_diff & mask) >> i;/*Extract the digit and push to 0 or 1*/
+		if (digit == 1)
+			differences++;
+	}
+	return (differences);
+}
