59abe1b6282c883e868928f94ab39316ed8ecd9b
Carlos Molano <926@holbertonschool.com>
2019-08-13 15:44:07 -0500

    Fixing file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,7 +12,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
 	{
 		write(STDERR_FILENO, "fails", 5);
@@ -24,12 +24,13 @@ int create_file(const char *filename, char *text_content)
 		for (i = 0; *(text_content + i) != '\0'; i++)
 			;
 		write(file, text_content, i);
+		close(file);
 	}
 	else
 	{
 		write(file, "", 0);
+		close(file);
 		return (-1);
 	}
-	close(file);
 	return (1);
 }
