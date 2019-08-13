7b236fc163fbfeaa16c6a1b8dc8f107a2310834a
Carlos Molano <926@holbertonschool.com>
2019-08-13 14:28:03 -0500

    Fixing file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,31 +10,27 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/*Allocate depends of num letters*/
 	char *print_field;
 	int file, read_file;
-	size_t i;
 
 	if (!filename)
 		return (0);
+
+	print_field = malloc((letters + 1) * sizeof(char));
+	if (print_field == NULL)
+		return (0);
+
+	*(print_field + letters) = '\0';
 
 	/*Open the file*/
 	file = open(filename, O_RDONLY);
 	if (file == -1)
 		return (0);
 
-	print_field = malloc(letters * sizeof(char));
-	if (print_field == NULL)
-		return (0);
-
 	/*Read the file and save in buffer*/
 	read_file = read(file, print_field, letters);
 
-	for (i = 0; i <= letters; i++)
-	{
-		write(STDOUT_FILENO, print_field, 1);
-		print_field++;
-	}
-
-	return (read_file);
+	write(STDOUT_FILENO, print_field, read_file);
 
 	close(file);
 	free(print_field);
+	return (read_file);
 }
