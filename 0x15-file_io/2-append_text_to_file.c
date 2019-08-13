d5af5a63b4291ad655c21b29f74c895bf219114c
Carlos Molano <926@holbertonschool.com>
2019-08-13 16:17:30 -0500

    Uploading file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * append_text_to_file - append text to the end of file
+ * @filename: name of file to be readed
+ * @text_content: the text that will be appended.
+ * Return: 2 success, otherwise 0
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_APPEND);
+	if (file == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; *(text_content + i) != '\0'; i++)
+			;
+		write(file, text_content, i);
+		close(file);
+	}
+	else
+	{
+		write(file, "", 0);
+		close(file);
+		return (-1);
+	}
+	return (1);
+}
