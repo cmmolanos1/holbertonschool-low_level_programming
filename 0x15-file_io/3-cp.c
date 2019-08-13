007fd6085287b11a87c277096821f9c760bcb873
Carlos Molano <926@holbertonschool.com>
2019-08-13 18:19:36 -0500

    uploading file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,32 +8,31 @@
  */
 int main(int argc, char **argv)
 {
-	int file_from, file_to;
+	int file_from, file_to, size;
 	char *buffer;
 
 	if (argc != 3)
 	{
-		exit(97);
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
-
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
 	{
-		exit(98);
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[0]);
+		exit(98);
 	}
 
-	file_to = open(argv[2], O_RDWR | O_CREAT | O_TRUNC, 0644);
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file_to == -1)
 	{
-		exit(99);
+		close(file_from);
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[1]);
+		exit(99);
 	}
-
 	buffer = malloc(1024);
-	read(file_from, buffer, 1024);
-	write(file_to, buffer, 1024);
+	size = read(file_from, buffer, sizeof(buffer));
+	write(file_to, buffer, size);
 	close(file_from);
 	close(file_to);
 	free(buffer);
