bb1a0e1a8cddcbce9791ce5ea3d3bb230f9ef819
Carlos Molano <926@holbertonschool.com>
2019-08-13 16:21:57 -0500

    uploading file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,10 +14,7 @@ int create_file(const char *filename, char *text_content)
 
 	file = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (file == -1)
-	{
-		write(STDERR_FILENO, "fails", 5);
 		return (-1);
-	}
 
 	if (text_content != NULL)
 	{
