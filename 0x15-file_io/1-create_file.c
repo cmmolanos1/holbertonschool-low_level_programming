05d7693940cd4c7384b114aa156c8ee684f052b4
Carlos Molano <926@holbertonschool.com>
2019-08-13 15:00:46 -0500

    Uloading 1-create_file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * create_file - creates a file
+ * @filename: name of file to be readed
+ * @text_content: the text inside the file.
+ * Return: 2 success, otherwise 0
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	file = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 600);
+	if (file == -1)
+	{
+		write(STDERR_FILENO, "fails", 5);
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		for (i = 0; *(text_content + i) != '\0'; i++)
+			;
+		write(file, text_content, i);
+	}
+	else
+		write(file, "", 1);
+
+	close(file);
+	return (1);
+}
