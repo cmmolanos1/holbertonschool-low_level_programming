b3ed833953ac8fe8a11ab712e25cb8687cbbf1d7
Carlos Molano <926@holbertonschool.com>
2019-09-15 18:25:56 -0500

    Uploading 8

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index -  delete a node in a idx position
+ * @head: pointer to head in function
+ * @index: index or position to be added
+ * Return: 1 success -1 failure
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int nodes;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+	current = *head;
+
+	for (nodes = 0; current != NULL && nodes < index; nodes++)
+		current = current->next;
+	if (current == NULL)
+		return (-1);
+	if (*head == current)
+		*head = current->next;
+	if (current->next != NULL)
+		current->next->prev = current->prev;
+	if (current->prev != NULL)
+		current->prev->next = current->next;
+	free(current);
+	return (1);
+}
