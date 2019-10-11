eca1a63593c8fcaf3733043f49f3cb00b188e72c
Carlos Molano <926@holbertonschool.com>
2019-10-11 06:20:38 -0500

    [Fix] 0. Size of pointer

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -22,7 +22,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table->size = size;
 
 	/*Allocate pointers to the head nodes*/
-	hash_table->array = malloc(sizeof(hash_node_t) * size);
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
 		free(hash_table);
