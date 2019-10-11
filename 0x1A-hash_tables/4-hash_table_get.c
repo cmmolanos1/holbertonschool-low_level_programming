a05b0dfcc286c7e893a05b83c50bc9a876d47898
Carlos Molano <926@holbertonschool.com>
2019-10-11 09:29:29 -0500

    [Fix] 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	int index = 0;
 	hash_node_t *node;
 
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
