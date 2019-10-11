140da20285394ca2b9da5e9512c035024967ca45
Carlos Molano <926@holbertonschool.com>
2019-10-11 08:07:03 -0500

    [Upload] 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table you want to look into
+ * @key: the key you are looking for
+ * Return: tha value if success otherwise NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	int index = 0;
+	hash_node_t *node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	node = ht->array[index];
+
+	while (node != NULL && node->key != NULL && strcmp(key, node->key) > 0)
+		node = node->next;
+
+	if (node == NULL || node->key == NULL || strcmp(key, node->key) != 0)
+		return (NULL);
+
+	return (node->value);
+}
