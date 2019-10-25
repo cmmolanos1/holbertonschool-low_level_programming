2a54b91ecac34e70c1f525381b8d8ae69efe2b2c
Carlos Molano <926@holbertonschool.com>
2019-10-25 11:30:30 -0500

    [Upload] 2

---
+++ 0x1B-sorting_algorithms/2-selection_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+/**
+ * selection_sort - perform an incremental sorting using selection
+ * sort method
+ * @array: array to be sorted
+ * @size: array's size
+ * Return: nothing
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min_index;
+	int swap;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min_index = i;
+
+		for (j = i + 1; j < size; j++)
+			if (array[j] < array[min_index])
+				min_index = j;
+
+		swap = array[min_index];
+		array[min_index] = array[i];
+		array[i] = swap;
+		print_array(array, size);
+	}
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -1,9 +1,7 @@
 #ifndef _SORT_H_
 #define _SORT_H_
 
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
+#include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,13 +12,15 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* _SORT_H_ */
