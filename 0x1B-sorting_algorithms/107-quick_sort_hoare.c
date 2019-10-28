549e305cbbd2f6e6664c7191fd96b13bb4feefbb
Carlos Molano <926@holbertonschool.com>
2019-10-28 14:57:25 -0500

    [Upload] 107

---
+++ 0x1B-sorting_algorithms/107-quick_sort_hoare.c
@@ -0,0 +1,66 @@
+#include "sort.h"
+#include <sys/types.h>
+/**
+ * partition - divides the array and swap datas
+ * @array: array to be sorted
+ * @low: first index
+ * @high: last index
+ * @size: array's size
+ * Return: nothing
+ */
+size_t partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[low], swap;
+	int i = low - 1, j = high + 1;
+
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
+		if (i >= j)
+			return (j);
+		swap = array[j];
+		array[j] = array[i];
+		array[i] = swap;
+		print_array(array, size);
+	}
+}
+/**
+ * quicksort - divides the array to perform the sorting
+ * sort method
+ * @array: array to be sorted
+ * @low: first index
+ * @high: last index
+ * @size: array's size
+ * Return: nothing
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	int p;
+
+	if (low < high)
+	{
+		p = partition(array, low, high, size);
+		quicksort(array, low, p, size);
+		quicksort(array, p + 1, high, size);
+	}
+}
+
+/**
+ * quick_sort_hoare - perform an incremental sorting using Hoare
+ * sort method
+ * @array: array to be sorted
+ * @size: array's size
+ * Return: nothing
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	quicksort(array, 0, size - 1, size);
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -29,5 +29,6 @@ void merge_sort(int *array, size_t size);
 void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif /* _SORT_H_ */
