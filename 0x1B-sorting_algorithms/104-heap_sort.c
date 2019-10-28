6a1e80275cd4d697e21042c405a3d33decf6003d
Carlos Molano <926@holbertonschool.com>
2019-10-28 11:28:32 -0500

    [Fix] 104: output

--- 0x1B-sorting_algorithms/104-heap_sort.c
+++ 0x1B-sorting_algorithms/104-heap_sort.c
@@ -3,31 +3,32 @@
  * heapify - modify the heap and sort
  * @array: array to be sorted
  * @size: array's size
+ * @n: size of heap
  * @i: root node
  * Return: nothing
  */
-void heapify(int *array, size_t size, size_t i)
+void heapify(int *array, size_t n, size_t i, size_t size)
 {
-	size_t largest = i;
-	size_t l = 2 * i + 1;
-	size_t r = 2 * i + 2;
+	size_t largest, left, right;
 	int swap;
 
-	if (l < size && array[l] > array[largest])
-		largest = l;
-
-	if (r < size && array[r] > array[largest])
-		largest = r;
-
+	largest = i;
+	left = 2 * i + 1;
+	right = 2 * i + 2;
+	if (left < n && array[left] > array[largest])
+		largest = left;
+	if (right < n && array[right] > array[largest])
+		largest = right;
 	if (largest != i)
 	{
 		swap = array[largest];
 		array[largest] = array[i];
 		array[i] = swap;
 		print_array(array, size);
-		heapify(array, size, largest);
+		heapify(array, n, largest, size);
 	}
 }
+
 
 /**
  * heap_sort - implementation of heap sort algorithm
@@ -37,22 +38,20 @@ void heapify(int *array, size_t size, size_t i)
  */
 void heap_sort(int *array, size_t size)
 {
-	register int i;
+	size_t i;
 	int swap;
 
-	if (array == NULL || size < 2)
+	if (!array || size < 2)
 		return;
-
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, size, i);
-
-	for (i = size - 1; i >= 0; i--)
+	for (i = (size - 2) / 2 + 1; i > 0; i--)
+		heapify(array, size, i - 1, size);
+	for (i = size; i > 0; i--)
 	{
-		swap = array[i];
-		array[i] = array[0];
+		swap = array[i - 1];
+		array[i - 1] = array[0];
 		array[0] = swap;
-		if (i != 0)
-			print_array(array, size);
-		heapify(array, i, 0);
+		print_array(array, size);
+		heapify(array, i - 1, 0, size);
 	}
 }
+
