a2da33d840f3a5a97897af25d860b29eea83312e
Carlos Molano <926@holbertonschool.com>
2019-10-28 19:22:25 -0500

    [Upload] 1

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,48 @@
+#include "sort.h"
+/**
+ * swap - swap nodes
+ * @a: first node
+ * @b: second node
+ * Return: nothing
+ */
+void swap(listint_t *a, listint_t *b)
+{
+	if (a->prev)
+		a->prev->next = b;
+	if (b->next)
+		b->next->prev = a;
+	a->next = b->next;
+	b->prev = a->prev;
+	a->prev = b;
+	b->next = a;
+}
+/**
+ * insertion_sort_list - perform an incremental sorting using insertion
+ * sort method
+ * @list: list to be sorted
+ * Return: nothing
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *i, *j;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+	i = (*list)->next;
+	while (i)
+	{
+		j = i;
+		while (j && j->prev)
+		{
+			if (j->prev->n > j->n)
+			{
+				swap(j->prev, j);
+				if (j->prev == NULL)
+					*list = j;
+				print_list((const listint_t *)*list);
+			}
+			j = j->prev;
+		}
+		i = i->next;
+	}
+}
